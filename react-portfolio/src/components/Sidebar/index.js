6251c48abd9986d51d8ac2c6ed11c469addc864f
TzuChiehChen <tzc005@ucsd.edu>
2024-04-30 13:30:37 -0700

    sidebar

--- react-portfolio/src/components/Sidebar/index.js
+++ react-portfolio/src/components/Sidebar/index.js
@@ -3,7 +3,7 @@ import './index.scss'
 import LogoJ from '../../assets/images/logo-j.png'
 import LogoSubtitle from '../../assets/images/logo-jay.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faHome} from '@fortawesome/free-solid-svg-icons'
+import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 
 
 const Sidebar = () => (
@@ -16,6 +16,12 @@ const Sidebar = () => (
     <nav>
         <NavLink exact="true" activeclassname="active" to="/">
             <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className = "about-Link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" className = "contact-Link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
     </nav>
 </div>
