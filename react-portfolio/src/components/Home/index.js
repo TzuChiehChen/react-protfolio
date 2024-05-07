00d637092c868f4eddeba187d3d839e3a46dc9b8
TzuChiehChen <tzc005@ucsd.edu>
2024-05-06 17:56:41 -0700

    home page animation

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -1,11 +1,20 @@
-import LogoTitle from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/logo-j.png'
+import { Link } from 'react-router-dom'
+import './index.scss';
+
 const Home = () => {
     
     return (
         <div className="ocntainer home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm</h1>
+                <h1>Hi, <br /> I'm
                 <img src={LogoTitle} alt="developer" />
+                ay
+                <br />
+                web developer
+                </h1>
+                <h2> Frontend Developer / UI Designer</h2>
+                <Link to = "/contact" className='flat-button'>CONTACT ME</Link>
             </div>
 
 
