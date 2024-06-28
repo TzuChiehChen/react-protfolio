37aa2bb3a555072ef6c448228672e4a8c3535f0b
TzuChiehChen <tzc005@ucsd.edu>
2024-06-27 19:50:27 -0700

    UI/ux designer

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -9,7 +9,7 @@ import Loader from 'react-loaders'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['J', 'a', 'y']
-    const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r', '.']
+    const jobArray = ['U', 'I', '/', 'U', 'X', ' ', 'D', 'e', 's', 'i', 'g', 'n', 'e', 'r']
 
     
     useEffect(() => {
