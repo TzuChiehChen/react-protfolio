e170a6b708a9231bf8f058749ceb954aa1bc9411
TzuChiehChen <tzc005@ucsd.edu>
2024-07-06 17:37:57 -0700

    home page done

--- react-portfolio/src/components/Home/index.js
+++ react-portfolio/src/components/Home/index.js
@@ -9,8 +9,9 @@ import Loader from 'react-loaders'
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['J', 'a', 'y']
-    const jobArray = ['U', 'I', '/', 'U', 'X', ' ', 'D', 'e', 's', 'i', 'g', 'n', 'e', 'r']
-
+    const jobArray = ['U', 'I', '/', 'U', 'X', ' ', 'D', 'e', 's', 'i', 'g', 'n', 'e', 'r', '|']
+    const jobArray1 = ['I', 'n', 't', 'e', 'r', 'a', 'c', 't', 'i', 'o', 'n', ' ', 'D', 'e', 's', 'i', 'g', 'n', '|']
+    const jobArray2 = ['F', 'r', 'o', 'n', 't', '-', 'E', 'n', 'd', ' ', 'D', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
     
     useEffect(() => {
         setTimeout(() => {
@@ -22,31 +23,45 @@ const Home = () => {
         <>
         <div className="container home-page">
             <div className="text-zone">
-                <h1>
-                <span className = {letterClass}>H</span>
-                <span className = {`${letterClass} _12`}>i,</span>
-                <br />
-                <span className = {`${letterClass} _13`}>I</span>
-                <span className = {`${letterClass} _14`}>'m</span>
-                <span className = {`${letterClass} _15`}> </span>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={nameArray}
-                idx={15} />
-                <br />
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22} />
-                </h1>
-                <h2> Frontend Developer / UI Designer</h2>
+            <h1 className="hi-section">
+                        <span className={letterClass}>H</span>
+                        <span className={`${letterClass} _12`}>i,</span>
+                    </h1>
+                    <h1 className="name-section">
+                        <span className={`${letterClass} _13`}>I</span>
+                        <span className={`${letterClass} _14`}>'m</span>
+                        <span className={`${letterClass} _15`}> </span>
+                        <AnimatedLetters letterClass={letterClass}
+                                         strArray={nameArray}
+                                         idx={15} />
+                    </h1>
+                <div>
+                    <p>
+                        I'm a passionate designer and front-end developer, based in California.
+                        I believe that great design is visually stunning and seamlessly functional,
+                        and I'm dedicated to creating user-centric experiences that leave a lasting impression.
+                    </p>
+                </div>
+            
                 <Link to = "/contact" className='flat-button'>CONTACT ME</Link>
-                
 
+                <h2 className="job-section"> 
+                <AnimatedLetters letterClass={letterClass}
+                                         strArray={jobArray}
+                                         idx={15} />
+                <AnimatedLetters letterClass={letterClass}
+                                         strArray={jobArray1}
+                                         idx={15} />   
+                <AnimatedLetters letterClass={letterClass}
+                                         strArray={jobArray2}
+                                         idx={15} />                                               
+                </h2>
             </div>
             <Logo />
         </div>
         <Loader type = "pacman" />
         </>
-    );
+    )
 
     
 }
